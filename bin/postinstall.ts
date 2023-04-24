eef9d15dfd8cda545557377e910ab91ce15fe648
Joern Meyer <joern.meyer@kernpunkt.de>
2023-04-24 08:56:44 +0200

    fix: postinstall

--- bin/postinstall.ts
+++ bin/postinstall.ts
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import * as path from "path";
 
 let rootDir = process.cwd();
-while (!fs.existsSync(path.join(rootDir, "package.json"))) {
+while (!existsSync(path.join(rootDir, "package.json"))) {
   rootDir = path.dirname(rootDir);
 }
-const packageJson = JSON.parse(
-  fs.readFileSync(path.join(rootDir, "package.json"))
-);
+const fileContent = readFileSync(path.join(rootDir, "package.json"));
+const packageJson = JSON.parse(fileContent.toString());
 
 packageJson.scripts["hush:push"] =
   "cd node_modules/@kernpunkt/hush && cdk deploy --context HushStack:secretsFile=../../../.env && cd -";
 packageJson.scripts["hush:pull"] =
   "node node_modules/@kernpunkt/hush/bin/hush.ts bin/hush.ts <secret> --file=./.env";
 
-fs.writeFileSync(
+writeFileSync(
   path.join(rootDir, "package.json"),
   JSON.stringify(packageJson, null, 2)
 );
