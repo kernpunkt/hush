112df0a80b3d63c8dc812a49f3d4d09c7913fcda
Joern Meyer <joern.meyer@kernpunkt.de>
2023-04-24 09:47:01 +0200

    fix: postinstall

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -8,8 +8,8 @@ const packageJson = JSON.parse(packageJsonFileContent);
 
 packageJson.scripts = packageJson.scripts || {};
 packageJson.scripts["hush:push"] =
-  "cd node_modules/@kernpunkt/hush && cdk deploy --context HushStack:secretsFile=../../../.env && cd -";
+  "cd node_modules/@kernpunkt/hush npm install && cdk deploy --context HushStack:secretsFile=../../../.env && cd -";
 packageJson.scripts["hush:pull"] =
-  "node node_modules/@kernpunkt/hush/bin/hush.ts bin/hush.ts <secret> --file=./.env";
+  "npx ts-node node_modules/@kernpunkt/hush/bin/hush.ts --file=./.env <secretName>";
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
