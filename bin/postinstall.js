868c3f8359ab406a5b8121db1046ec426398dc02
Joern Meyer <joern.meyer@kernpunkt.de>
2023-04-24 10:20:54 +0200

    fix: don't overwrite scripts

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -8,8 +8,10 @@ const packageJson = JSON.parse(packageJsonFileContent);
 
 packageJson.scripts = packageJson.scripts || {};
 packageJson.scripts["hush:push"] =
+  packageJson.scripts["hush:push"] ||
   "cd node_modules/@kernpunkt/hush && npm install && cdk deploy --context secretsFile=../../../.env --context envName=<your-project>-prod && cd -";
 packageJson.scripts["hush:pull"] =
+  packageJson.scripts["hush:pull"] ||
   "npx ts-node node_modules/@kernpunkt/hush/bin/hush.ts --file=./.env <secretName>";
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
