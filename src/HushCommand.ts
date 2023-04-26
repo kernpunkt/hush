3aa5cb3b10823408f29fdcff5c499b283f428f97
Joern Meyer <joern.meyer@kernpunkt.de>
2023-04-26 07:49:37 +0200

    style: refactored command generation

---
+++ src/HushCommand.ts
@@ -0,0 +1,88 @@
+import { Command } from "commander";
+import PushCommand from "./commands/PushCommand";
+import chalk from "chalk";
+import PullCommand, { PullCommandOptions } from "./commands/PullCommand";
+import { EnvDiffResult } from "./utils/envDiff";
+
+class HushCommand extends Command {
+  constructor() {
+    super();
+    this.name("hush").argument("<command>", "The command to run.");
+    this.pushCommand();
+    this.pullCommand();
+  }
+
+  private pushCommand(): void {
+    this.command("push")
+      .argument(
+        "<key>",
+        "A designator to store this secret as. Can be something like 'customer-prod' or 'your-name'. All keys get prefixed with 'hush-'"
+      )
+      .argument(
+        "<env-file>",
+        "Path to the .env file containing the secrets you want to push."
+      )
+      .action(async (key: string, envFile: string) => {
+        console.log(`${chalk.bold("Hush! 🤫")} — Push\n`);
+        const command = new PushCommand(key, envFile);
+        await command.execute();
+      });
+  }
+
+  private pullCommand(): void {
+    this.command("pull")
+      .argument(
+        "<key>",
+        "The designator you stored your secret with. Remember that it was prefixed with 'hush-'."
+      )
+      .argument(
+        "<env-file>",
+        "Path to the .env file containing the secrets you want to push."
+      )
+      .option(
+        "-f, --force",
+        "Force the destination file to be overwritten, even if changes are incoming."
+      )
+      .action(async (key, envFile: string, options: PullCommandOptions) => {
+        console.log(`${chalk.bold("Hush! 🤫")} — Pull\n`);
+
+        const command = new PullCommand(key, envFile, options);
+        command
+          .execute()
+          .then((result) => {
+            if (typeof result === "object") {
+              const changeset = result as EnvDiffResult;
+              console.log(
+                `${chalk.bold.yellow(
+                  "⚠️ Warning!"
+                )} Your .env file and the .env file stored in AWS differ:\n`
+              );
+              changeset.added.map((item) =>
+                console.log(chalk.green(`\t[ADD   ] ${item}`))
+              );
+              changeset.removed.map((item) =>
+                console.log(chalk.red(`\t[REMOVE] ${item}`))
+              );
+              changeset.changed.map((item) =>
+                console.log(chalk.yellow(`\t[CHANGE] ${item}`))
+              );
+              console.log(
+                `${chalk.bold(
+                  "\nIf you're sure you want to incorporate those diffs, rerun this command with the --force flag."
+                )}`
+              );
+              process.exit(1);
+            }
+
+            console.log(result);
+            process.exit(0);
+          })
+          .catch((err) => {
+            console.error(err);
+            process.exit(1);
+          });
+      });
+  }
+}
+
+export default HushCommand;
--- src/hush.ts
+++ src/hush.ts
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import PushCommand from "./commands/PushCommand";
-import PullCommand, { PullCommandOptions } from "./commands/PullCommand";
 import chalk from "chalk";
-import { EnvDiffResult } from "./utils/envDiff";
+import HushCommand from "./HushCommand";
 
 const awsProfile = process.env["AWS_PROFILE"];
 if (!awsProfile) {
@@ -17,77 +14,5 @@ if (!awsProfile) {
   process.exit(1);
 }
 
-const program = new Command();
-program.name("hush").argument("<command>", "The command to run.");
-
-program
-  .command("push")
-  .argument(
-    "<key>",
-    "A designator to store this secret as. Can be something like 'customer-prod' or 'your-name'. All keys get prefixed with 'hush-'"
-  )
-  .argument(
-    "<env-file>",
-    "Path to the .env file containing the secrets you want to push."
-  )
-  .action(async (key: string, envFile: string) => {
-    console.log(`${chalk.bold("Hush! 🤫")} — Push\n`);
-    const command = new PushCommand(key, envFile);
-    await command.execute();
-  });
-
-program
-  .command("pull")
-  .argument(
-    "<key>",
-    "The designator you stored your secret with. Remember that it was prefixed with 'hush-'."
-  )
-  .argument(
-    "<env-file>",
-    "Path to the .env file containing the secrets you want to push."
-  )
-  .option(
-    "-f, --force",
-    "Force the destination file to be overwritten, even if changes are incoming."
-  )
-  .action(async (key, envFile: string, options: PullCommandOptions) => {
-    console.log(`${chalk.bold("Hush! 🤫")} — Pull\n`);
-
-    const command = new PullCommand(key, envFile, options);
-    command
-      .execute()
-      .then((result) => {
-        if (typeof result === "object") {
-          const changeset = result as EnvDiffResult;
-          console.log(
-            `${chalk.bold.yellow(
-              "⚠️ Warning!"
-            )} Your .env file and the .env file stored in AWS differ:\n`
-          );
-          changeset.added.map((item) =>
-            console.log(chalk.green(`\t[ADD   ] ${item}`))
-          );
-          changeset.removed.map((item) =>
-            console.log(chalk.red(`\t[REMOVE] ${item}`))
-          );
-          changeset.changed.map((item) =>
-            console.log(chalk.yellow(`\t[CHANGE] ${item}`))
-          );
-          console.log(
-            `${chalk.bold(
-              "\nIf you're sure you want to incorporate those diffs, rerun this command with the --force flag."
-            )}`
-          );
-          process.exit(1);
-        }
-
-        console.log(result);
-        process.exit(0);
-      })
-      .catch((err) => {
-        console.error(err);
-        process.exit(1);
-      });
-  });
-
-program.parse();
+const hush = new HushCommand();
+hush.parse();
