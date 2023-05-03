db533add6072410eb14736d1b3b604974f8e21ce
Joern Meyer <joern.meyer@kernpunkt.de>
2023-05-03 10:58:59 +0200

    refactor: getClient() no longer needed in Base class

--- src/commands/BaseCommand.ts
+++ src/commands/BaseCommand.ts
@@ -1,16 +1,8 @@
-import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
-
 abstract class BaseCommand {
   protected key: string;
 
   protected getKey(): string {
     return `hush-${this.key}`;
-  }
-
-  protected getClient(): SecretsManagerClient {
-    return new SecretsManagerClient({
-      region: "eu-central-1",
-    });
   }
 
   public abstract execute(): void;
